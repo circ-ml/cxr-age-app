db51e9dc01a1b35dc613116f28ac6d6912e884a9
xtrp <36894883+xtrp@users.noreply.github.com>
2021-11-06 22:49:13 -0400

    Fix issue with loader taking up entire width on mobile

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,18 +1,45 @@
+import React from "react";
 import BarLoader from "react-spinners/BarLoader";
 
-export default function Loader() {
-  return (
-    <div className="w-full h-48 flex flex-col justify-center align-center">
-      <h1 className="mb-4 text-blue-600 text-md uppercase tracking-widest font-semibold text-center">
-        Loading Results...
-      </h1>
-      <BarLoader
-        css="display: block; margin: 0 auto;"
-        color={"#2563eb"}
-        loading={true}
-        width={350}
-        height={8}
-      />
-    </div>
-  );
+const DEFAULT_LOADER_WIDTH = 350;
+const MAX_LOADER_WIDTH_VW = 60;
+
+export default class Loader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaderWidth: DEFAULT_LOADER_WIDTH,
+    };
+    this.updateLoaderWidth = this.updateLoaderWidth.bind(this);
+  }
+  componentDidMount() {
+    this.updateLoaderWidth();
+    window.addEventListener("resize", this.updateLoaderWidth);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateLoaderWidth);
+  }
+  updateLoaderWidth() {
+    const loaderWidth = Math.min(
+      DEFAULT_LOADER_WIDTH,
+      window.innerWidth * (MAX_LOADER_WIDTH_VW / 100)
+    );
+    this.setState({ loaderWidth });
+  }
+  render() {
+    return (
+      <div className="w-full h-48 flex flex-col justify-center align-center">
+        <h1 className="mb-4 text-blue-600 text-md uppercase tracking-widest font-semibold text-center">
+          Loading Results...
+        </h1>
+        <BarLoader
+          css="display: block; margin: 0 auto;"
+          color={"#2563eb"}
+          loading={true}
+          width={this.state.loaderWidth}
+          height={8}
+        />
+      </div>
+    );
+  }
 }
